05253abc94680eb353a0c50e470c1a24f0d80928
Valery Bugakov <skymk1@gmail.com>
2021-07-23 17:13:18 +0300

    feat: remove unreachable code

--- src/transforms/globalCssToCssModule/postcss/transformFileToCssModule.ts
+++ src/transforms/globalCssToCssModule/postcss/transformFileToCssModule.ts
@@ -1,4 +1,3 @@
-import { writeFileSync, rmSync } from 'fs'
 import path from 'path'
 
 import { createCssProcessor } from './createCssProcessor'
@@ -21,12 +20,6 @@ export async function transformFileToCssModule(
     const newFilePath = path.join(dir, `${name}.module.scss`)
 
     // TODO: add option to write files.
-    // eslint-disable-next-line no-constant-condition
-    if (false) {
-        writeFileSync(newFilePath, transformedResult.css)
-        rmSync(sourceFilePath)
-    }
-
     return {
         css: transformedResult.css,
         filePath: newFilePath,
