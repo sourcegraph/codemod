c74a55b6329c03bbb11d5cb5969423c1e0e3dec9
Valery Bugakov <skymk1@gmail.com>
2021-10-12 19:08:50 +0300

    chore: add empty buton codemod

--- src/transforms/index.ts
+++ src/transforms/index.ts
@@ -1,7 +1,9 @@
+import { buttonElementToComponent } from './buttonElementToComponent'
 import { classNameTemplateToClassnamesCall } from './classNameTemplateToClassnamesCall'
 import { globalCssToCssModule } from './globalCssToCssModule/globalCssToCssModule'
 
 export const transforms = {
+    buttonElementToComponent,
     globalCssToCssModule,
     classNameTemplateToClassnamesCall,
 } as const
