a59143f7126c7dd2b089c57896a863b3fa71b01c
Valery Bugakov <skymk1@gmail.com>
2022-04-28 00:36:42 -0700

    chore: add changesets (#103)

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -1,6 +1,7 @@
 import type { InitialOptionsTsJest } from 'ts-jest'
 
 const config: InitialOptionsTsJest = {
+    passWithNoTests: true,
     preset: 'ts-jest',
     globals: {
         'ts-jest': {
