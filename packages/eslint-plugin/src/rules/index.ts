435d386eb3b9f5372b7b190bb4b30acdb513531b
Valery Bugakov <skymk1@gmail.com>
2022-05-06 14:14:52 +0800

    chore: disble `use-button-component` rule

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -1,9 +1,7 @@
 // This file is used by `scripts/generate-configs.ts` for rules extraction.
 import { checkHelpLinks } from './check-help-links'
-import { useButtonComponent } from './use-button-component'
 
 // eslint-disable-next-line import/no-default-export
 export default {
-    'use-button-component': useButtonComponent,
     'check-help-links': checkHelpLinks,
 }
