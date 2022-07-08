02f788ce1a72a3db67c95129a2f6a566f0c202b1
Mihir Chaturvedi <plibither8@gmail.com>
2022-07-08 12:59:42 +0530

    lint: Add linter rule to ban uncommented empty catch blocks

--- packages/eslint-plugin/src/rules/index.ts
+++ packages/eslint-plugin/src/rules/index.ts
@@ -1,7 +1,9 @@
 // This file is used by `scripts/generate-configs.ts` for rules extraction.
 import { checkHelpLinks } from './check-help-links'
+import { noEmptyCatchBlocks } from './no-empty-catch-blocks'
 
 // eslint-disable-next-line import/no-default-export
 export default {
     'check-help-links': checkHelpLinks,
+    'no-empty-catch-blocks': noEmptyCatchBlocks
 }
---
+++ packages/eslint-plugin/src/rules/no-empty-catch-blocks/no-empty-catch-blocks.ts
@@ -0,0 +1,40 @@
+
+import { createRule } from '../../utils'
+
+export const messages = {
+    noEmptyCatchBlocks: 'Catch blocks should not be empty. Either process the error, or comment why the exception can be ignored',
+}
+
+export const noEmptyCatchBlocks = createRule<[], keyof typeof messages>({
+    name: 'no-empty-catch-blocks',
+    meta: {
+        docs: {
+            description: 'Bans usage of the empty catch blocks, unless supported by a comment explaining why the exception can be ignored.',
+            recommended: 'error',
+        },
+        messages,
+        schema: [],
+        type: 'problem',
+    },
+    defaultOptions: [],
+    create(context) {
+        const sourceCode = context.getSourceCode()
+
+        return {
+            CatchClause(node) {
+                // Get all comments inside the catch block, and right before it
+                const comments = [
+                    ...sourceCode.getCommentsBefore(node),
+                    ...sourceCode.getCommentsInside(node)
+                ]
+                // If the catch body is empty and there are no comments, report error
+                if (node.body.body.length === 0 && comments.length === 0) {
+                    context.report({
+                        node,
+                        messageId: 'noEmptyCatchBlocks',
+                    })
+                }
+            }
+        }
+    },
+})
